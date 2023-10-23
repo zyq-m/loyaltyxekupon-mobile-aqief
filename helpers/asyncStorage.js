a3fd1b757d127c1bcc2e2d57657f764ff8e4fb72
Haziq Musa <last.game01987@gmail.com>
2023-10-23 23:33:19 +0800

    create async storage helper

---
+++ helpers/asyncStorage.js
@@ -0,0 +1,42 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+// Store string
+export const storeData = async (key, value) => {
+  try {
+    await AsyncStorage.setItem(key, value);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// Get string
+export const getData = async key => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    if (value !== null) {
+      return value;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// Store object
+export const storeObject = async (key, value) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem(key, jsonValue);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// Get object
+export const getObject = async key => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(key);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    console.log(e);
+  }
+};
