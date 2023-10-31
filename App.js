b1a89c227ffe3181889165e63bea202905cd49e8
zyq-m <last.game01987@gmail.com>
2023-10-31 15:36:56 +0800

    devide screen based on role

--- App.js
+++ App.js
@@ -60,7 +60,8 @@ function StudentDrawerNavigator() {
         drawerStyle: { paddingTop: 16 },
         drawerActiveTintColor: "rgba(88, 83, 76, 1)",
         headerStyle: { backgroundColor: "#FFD400" },
-      }}>
+      }}
+    >
       <StudentDrawer.Screen
         name="DashboardDrawer" // Unique name DrawerNavigator
         component={Dashboard}
@@ -104,7 +105,8 @@ function CafeOwnerDrawerNavigator() {
         drawerStyle: { paddingTop: 16 },
         drawerActiveTintColor: "rgba(88, 83, 76, 1)",
         headerStyle: { backgroundColor: "#FFD400" },
-      }}>
+      }}
+    >
       <CafeOwnerDrawer.Screen
         name="CODashboardDrawer" // Unique name DrawerNavigator
         component={CODashboard}
@@ -156,7 +158,8 @@ function B40StudentDrawerNavigator() {
         drawerStyle: { paddingTop: 16 },
         drawerActiveTintColor: "rgba(88, 83, 76, 1)",
         headerStyle: { backgroundColor: "#FFD400" },
-      }}>
+      }}
+    >
       <B40StudentDrawer.Screen
         name="DashboardDrawer" // Unique name DrawerNavigator
         component={B40Dashboard}
@@ -217,7 +220,8 @@ export default function App() {
           screenOptions={{
             headerStyle: { backgroundColor: "#FFD400" },
             animation: "fade_from_bottom",
-          }}>
+          }}
+        >
           {!user?.isSignedIn ? (
             <>
               <Stack.Screen
@@ -228,199 +232,225 @@ export default function App() {
             </>
           ) : (
             <>
-              <Stack.Screen
-                name="Dashboard"
-                component={StudentDrawerNavigator}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="CollectPoint"
-                component={CollectPoint}
-                options={{ headerTitle: "Collect Point", headerShown: true }}
-              />
-              <Stack.Screen
-                name="CashlessCampaign"
-                component={CashlessCampaign}
-                options={{
-                  headerTitle: "Cashless Campaign",
-                  headerShown: true,
-                }}
-              />
-              <Stack.Screen
-                name="QRCashlessCampaign"
-                component={QRCashlessCampaign}
-                options={{
-                  headerTitle: "Scan QR(Cashless Campaign)",
-                  headerShown: true,
-                }}
-              />
-              <Stack.Screen
-                name="PINCashlessCampaign"
-                component={PINCashlessCampaign}
-                options={{
-                  headerTitle: "Insert PIN(Cashless Campaign)",
-                  headerShown: true,
-                }}
-              />
-              <Stack.Screen
-                name="GreenCampusCampaign"
-                component={GreenCampusCampaign}
-                options={{
-                  headerTitle: "Green Campus Campaign",
-                  headerShown: true,
-                }}
-              />
-              <Stack.Screen
-                name="QRGreenCampusCampaign"
-                component={QRGreenCampusCampaign}
-                options={{
-                  headerTitle: "Scan QR(Green Campus Campaign)",
-                  headerShown: true,
-                }}
-              />
-              <Stack.Screen
-                name="PINGreenCampusCampaign"
-                component={PINGreenCampusCampaign}
-                options={{
-                  headerTitle: "Insert PIN(Green Campus Campaign)",
-                  headerShown: true,
-                }}
-              />
-              <Stack.Screen
-                name="ClaimReward"
-                component={ClaimReward}
-                options={{ headerTitle: "Claim Reward", headerShown: true }}
-              />
-              <Stack.Screen
-                name="CODashboard"
-                component={CafeOwnerDrawerNavigator}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="COShowQR"
-                component={COShowQR}
-                options={{ headerTitle: "My QRCode", headerShown: true }}
-              />
-              <Stack.Screen
-                name="COShowPIN"
-                component={COShowPIN}
-                options={{
-                  headerTitle: "One-Time PIN Generator",
-                  headerShown: true,
-                }}
-              />
-              <Stack.Screen
-                name="MyQReKupon"
-                component={MyQReKupon}
-                options={{ headerTitle: "MyQR eKupon", headerShown: true }}
-              />
-              <Stack.Screen
-                name="MyQRCashless"
-                component={MyQRCashless}
-                options={{ headerTitle: "MyQR Cashless", headerShown: true }}
-              />
-              <Stack.Screen
-                name="MyQRGreenCampus"
-                component={MyQRGreenCampus}
-                options={{
-                  headerTitle: "MyQR Green Campus",
-                  headerShown: true,
-                }}
-              />
-              <Stack.Screen
-                name="MyPINGeneratorCashless"
-                component={MyPINGeneratorCashless}
-                options={{
-                  headerTitle: "OTP Generator(Cashless)",
-                  headerShown: true,
-                }}
-              />
-              <Stack.Screen
-                name="MyPINGeneratorGreenCampus"
-                component={MyPINGeneratorGreenCampus}
-                options={{
-                  headerTitle: "OTP Generator(Green Campus)",
-                  headerShown: true,
-                }}
-              />
-              <Stack.Screen
-                name="B40Dashboard"
-                component={B40StudentDrawerNavigator}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="PayNow"
-                component={PayNow}
-                options={{ headerTitle: "Choose amount", headerShown: true }}
-              />
-              <Stack.Screen
-                name="QRScan"
-                component={QRScan}
-                options={{ headerShown: false }}
-              />
-              <Stack.Screen
-                name="PayCafeWeb"
-                component={PayCafeWeb}
-                options={{ headerTitle: "Select Cafe", headerShown: true }}
-              />
-              <Stack.Screen
-                name="B40CollectPoint"
-                component={B40CollectPoint}
-                options={{ headerTitle: "Collect Point", headerShown: true }}
-              />
-              <Stack.Screen
-                name="B40ClaimReward"
-                component={B40ClaimReward}
-                options={{ headerTitle: "Claim Reward", headerShown: true }}
-              />
-              <Stack.Screen
-                name="B40CashlessCampaign"
-                component={B40CashlessCampaign}
-                options={{
-                  headerTitle: "Cashless Campaign",
-                  headerShown: true,
-                }}
-              />
-              <Stack.Screen
-                name="B40GreenCampusCampaign"
-                component={B40GreenCampusCampaign}
-                options={{
-                  headerTitle: "Green Campus Campaign",
-                  headerShown: true,
-                }}
-              />
-              <Stack.Screen
-                name="B40PINCashlessCampaign"
-                component={B40PINCashlessCampaign}
-                options={{
-                  headerTitle: "PIN (Cashless Campaign)",
-                  headerShown: true,
-                }}
-              />
-              <Stack.Screen
-                name="B40PINGreenCampusCampaign"
-                component={B40PINGreenCampusCampaign}
-                options={{
-                  headerTitle: "PIN (Green Campus Campaign)",
-                  headerShown: true,
-                }}
-              />
-              <Stack.Screen
-                name="B40QRCashlessCampaign"
-                component={B40QRCashlessCampaign}
-                options={{
-                  headerTitle: "QR (Cashless Campaign)",
-                  headerShown: true,
-                }}
-              />
-              <Stack.Screen
-                name="B40QRGreenCampusCampaign"
-                component={B40QRGreenCampusCampaign}
-                options={{
-                  headerTitle: "QR (Green Campus Campaign)",
-                  headerShown: true,
-                }}
-              />
+              {user?.role === "NON-B40" && (
+                <>
+                  <Stack.Screen
+                    name="Dashboard"
+                    component={StudentDrawerNavigator}
+                    options={{ headerShown: false }}
+                  />
+                  <Stack.Screen
+                    name="CollectPoint"
+                    component={CollectPoint}
+                    options={{
+                      headerTitle: "Collect Point",
+                      headerShown: true,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="CashlessCampaign"
+                    component={CashlessCampaign}
+                    options={{
+                      headerTitle: "Cashless Campaign",
+                      headerShown: true,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="QRCashlessCampaign"
+                    component={QRCashlessCampaign}
+                    options={{
+                      headerTitle: "Scan QR(Cashless Campaign)",
+                      headerShown: true,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="PINCashlessCampaign"
+                    component={PINCashlessCampaign}
+                    options={{
+                      headerTitle: "Insert PIN(Cashless Campaign)",
+                      headerShown: true,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="GreenCampusCampaign"
+                    component={GreenCampusCampaign}
+                    options={{
+                      headerTitle: "Green Campus Campaign",
+                      headerShown: true,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="QRGreenCampusCampaign"
+                    component={QRGreenCampusCampaign}
+                    options={{
+                      headerTitle: "Scan QR(Green Campus Campaign)",
+                      headerShown: true,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="PINGreenCampusCampaign"
+                    component={PINGreenCampusCampaign}
+                    options={{
+                      headerTitle: "Insert PIN(Green Campus Campaign)",
+                      headerShown: true,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="ClaimReward"
+                    component={ClaimReward}
+                    options={{ headerTitle: "Claim Reward", headerShown: true }}
+                  />
+                </>
+              )}
+
+              {user?.role === "CAFE" && (
+                <>
+                  <Stack.Screen
+                    name="CODashboard"
+                    component={CafeOwnerDrawerNavigator}
+                    options={{ headerShown: false }}
+                  />
+                  <Stack.Screen
+                    name="COShowQR"
+                    component={COShowQR}
+                    options={{ headerTitle: "My QRCode", headerShown: true }}
+                  />
+                  <Stack.Screen
+                    name="COShowPIN"
+                    component={COShowPIN}
+                    options={{
+                      headerTitle: "One-Time PIN Generator",
+                      headerShown: true,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="MyQReKupon"
+                    component={MyQReKupon}
+                    options={{ headerTitle: "MyQR eKupon", headerShown: true }}
+                  />
+                  <Stack.Screen
+                    name="MyQRCashless"
+                    component={MyQRCashless}
+                    options={{
+                      headerTitle: "MyQR Cashless",
+                      headerShown: true,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="MyQRGreenCampus"
+                    component={MyQRGreenCampus}
+                    options={{
+                      headerTitle: "MyQR Green Campus",
+                      headerShown: true,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="MyPINGeneratorCashless"
+                    component={MyPINGeneratorCashless}
+                    options={{
+                      headerTitle: "OTP Generator(Cashless)",
+                      headerShown: true,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="MyPINGeneratorGreenCampus"
+                    component={MyPINGeneratorGreenCampus}
+                    options={{
+                      headerTitle: "OTP Generator(Green Campus)",
+                      headerShown: true,
+                    }}
+                  />
+                </>
+              )}
+
+              {user?.role === "B40" && (
+                <>
+                  <Stack.Screen
+                    name="B40Dashboard"
+                    component={B40StudentDrawerNavigator}
+                    options={{ headerShown: false }}
+                  />
+                  <Stack.Screen
+                    name="PayNow"
+                    component={PayNow}
+                    options={{
+                      headerTitle: "Choose amount",
+                      headerShown: true,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="QRScan"
+                    component={QRScan}
+                    options={{ headerShown: false }}
+                  />
+                  <Stack.Screen
+                    name="PayCafeWeb"
+                    component={PayCafeWeb}
+                    options={{ headerTitle: "Select Cafe", headerShown: true }}
+                  />
+                  <Stack.Screen
+                    name="B40CollectPoint"
+                    component={B40CollectPoint}
+                    options={{
+                      headerTitle: "Collect Point",
+                      headerShown: true,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="B40ClaimReward"
+                    component={B40ClaimReward}
+                    options={{ headerTitle: "Claim Reward", headerShown: true }}
+                  />
+                  <Stack.Screen
+                    name="B40CashlessCampaign"
+                    component={B40CashlessCampaign}
+                    options={{
+                      headerTitle: "Cashless Campaign",
+                      headerShown: true,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="B40GreenCampusCampaign"
+                    component={B40GreenCampusCampaign}
+                    options={{
+                      headerTitle: "Green Campus Campaign",
+                      headerShown: true,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="B40PINCashlessCampaign"
+                    component={B40PINCashlessCampaign}
+                    options={{
+                      headerTitle: "PIN (Cashless Campaign)",
+                      headerShown: true,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="B40PINGreenCampusCampaign"
+                    component={B40PINGreenCampusCampaign}
+                    options={{
+                      headerTitle: "PIN (Green Campus Campaign)",
+                      headerShown: true,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="B40QRCashlessCampaign"
+                    component={B40QRCashlessCampaign}
+                    options={{
+                      headerTitle: "QR (Cashless Campaign)",
+                      headerShown: true,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="B40QRGreenCampusCampaign"
+                    component={B40QRGreenCampusCampaign}
+                    options={{
+                      headerTitle: "QR (Green Campus Campaign)",
+                      headerShown: true,
+                    }}
+                  />
+                </>
+              )}
             </>
           )}
         </Stack.Navigator>
