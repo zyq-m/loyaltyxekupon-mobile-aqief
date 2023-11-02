83b15bda29b588fb68f9cc050e478f033d43846f
zyq-m <last.game01987@gmail.com>
2023-11-02 14:55:38 +0800

    fetch cafe list

--- pages/b40-student/PayCafeWeb.js
+++ pages/b40-student/PayCafeWeb.js
@@ -10,90 +10,6 @@ const PayCafeWeb = () => {
   const navigation = useNavigation();
   const [radioBtn, setRadioBtn] = useState([]);
 
-  useEffect(() => {
-    // Initialize the radio buttons state with the local cafe data
-    setRadioBtn(localCafeData);
-  }, []);
-
-  const localCafeData = [
-    { id: 1, label: "Starbucks Malaysia", value: "starbucks", selected: false },
-    {
-      id: 2,
-      label: "Coffee Bean & Tea Leaf",
-      value: "coffeebean",
-      selected: false,
-    },
-    { id: 3, label: "OldTown White Coffee", value: "oldtown", selected: false },
-    { id: 4, label: "Dome Cafe", value: "domecafe", selected: false },
-    { id: 5, label: "PappaRich", value: "papparich", selected: false },
-    { id: 6, label: "Tealive", value: "tealive", selected: false },
-    {
-      id: 7,
-      label: "Secret Recipe Cafe",
-      value: "secretrecipe",
-      selected: false,
-    },
-    {
-      id: 8,
-      label: "San Francisco Coffee",
-      value: "sanfrancisco",
-      selected: false,
-    },
-    { id: 9, label: "Marrybrown Cafe", value: "marrybrown", selected: false },
-    { id: 10, label: "The Alley", value: "thealley", selected: false },
-    { id: 11, label: "Gong Cha", value: "gongcha", selected: false },
-    {
-      id: 12,
-      label: "Kenny Rogers Roasters",
-      value: "kennyrogers",
-      selected: false,
-    },
-    { id: 13, label: "Toast Box", value: "toastbox", selected: false },
-    {
-      id: 14,
-      label: "Pacific Coffee",
-      value: "pacificcoffee",
-      selected: false,
-    },
-    { id: 15, label: "Chatime", value: "chatime", selected: false },
-    { id: 16, label: "Nescafe Cafe", value: "nescafe", selected: false },
-    { id: 17, label: "Coffee Zone", value: "coffeezone", selected: false },
-    { id: 18, label: "Coffee Lab", value: "coffeelab", selected: false },
-    {
-      id: 19,
-      label: "Coffee Culture",
-      value: "coffeeculture",
-      selected: false,
-    },
-    { id: 20, label: "Coffee Heaven", value: "coffeeheaven", selected: false },
-    { id: 21, label: "Brew & Bread", value: "brewbread", selected: false },
-    {
-      id: 22,
-      label: "Black Canyon Coffee",
-      value: "blackcanyon",
-      selected: false,
-    },
-    { id: 23, label: "Coffee World", value: "coffeeworld", selected: false },
-    {
-      id: 24,
-      label: "Coffee Chemistry Signature",
-      value: "coffeechemistry",
-      selected: false,
-    },
-    {
-      id: 25,
-      label: "Pacific Coffee",
-      value: "pacificcoffee",
-      selected: false,
-    },
-    { id: 26, label: "The Coffee Club", value: "coffeecafe", selected: false },
-    { id: 27, label: "Brew & Stone", value: "brewstone", selected: false },
-    { id: 28, label: "Brewyard Coffee", value: "brewyard", selected: false },
-    { id: 29, label: "Cafe Barbera", value: "barberacafe", selected: false },
-    { id: 30, label: "Cafe Amazon", value: "cafeamazon", selected: false },
-    // Add more cafes as needed
-  ];
-
   const toDashboard = () => {
     // You can add your authentication logic here
     // If authentication is successful, navigate to the "Dashboard" screen
@@ -113,7 +29,14 @@ const PayCafeWeb = () => {
     api
       .get("/student/cafe")
       .then((res) => {
-        console.log(res);
+        let cafeList = res.data.data.map((data, i) => ({
+          id: i,
+          label: data.name,
+          value: data.id,
+          selected: false,
+        }));
+
+        setRadioBtn(cafeList);
       })
       .catch((err) => {
         console.error(err);
