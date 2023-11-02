b3f5a9ba1e6158a6193b63d1db83ec0909080580
zyq-m <last.game01987@gmail.com>
2023-11-02 15:57:17 +0800

    create pay function

--- pages/b40-student/PayCafeWeb.js
+++ pages/b40-student/PayCafeWeb.js
@@ -4,16 +4,37 @@ import { RadioButton } from "react-native-radio-buttons-group";
 import { Button, Refresh } from "../../components";
 import { globals } from "../../styles";
 import { useNavigation } from "@react-navigation/native";
-import { api } from "../../services/axiosInstance";
 
-const PayCafeWeb = () => {
+import { api } from "../../services/axiosInstance";
+import { useUserContext } from "../../hooks/useUserContext";
+import { socket } from "../../services/socketInstance";
+
+const PayCafeWeb = ({ route }) => {
   const navigation = useNavigation();
+  const { user } = useUserContext();
+  const { amount } = route.params;
   const [radioBtn, setRadioBtn] = useState([]);
 
-  const toDashboard = () => {
-    // You can add your authentication logic here
-    // If authentication is successful, navigate to the "Dashboard" screen
-    navigation.navigate("B40Dashboard"); // Replace "Dashboard" with your route name
+  const onPay = async () => {
+    const selectedCafe = radioBtn.filter(({ selected }) => selected === true);
+
+    try {
+      await api.post("/student/pay", {
+        matricNo: user?.id,
+        cafeId: selectedCafe[0].value,
+        amount: amount,
+      });
+      // push notification then nav to dashboard
+
+      // emit event
+      socket.emit("student:get-wallet-total", { matricNo: user?.id });
+      socket.emit("admin:get-overall");
+
+      navigation.navigate("B40Dashboard"); // Replace "Dashboard" with your route name
+      // console.log({ amount: amount, select: selectedCafe[0].value });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const handleRadioButtonChange = (id) => {
@@ -62,7 +83,7 @@ const PayCafeWeb = () => {
         })}
       </ScrollView>
       <View style={{ paddingBottom: 24, paddingHorizontal: 16 }}>
-        <Button label={"Pay"} onPress={toDashboard} />
+        <Button label={"Pay"} onPress={onPay} />
       </View>
     </View>
   );
