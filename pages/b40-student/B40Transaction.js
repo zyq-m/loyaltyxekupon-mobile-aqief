356030e0a222452b8e6bf29b4481c2d0f89d5db1
zyq-m <last.game01987@gmail.com>
2023-11-03 00:18:35 +0800

    update transaction

--- pages/b40-student/B40Transaction.js
+++ pages/b40-student/B40Transaction.js
@@ -4,8 +4,24 @@ import { globals, transactionStyle } from "../../styles";
 import TransactionContainer from "../../components/TransactionContainer";
 import B40TransactionList from "../../components/B40TransactionList";
 import B40dummyData from "../../components/B40dummyData";
+import { useEffect, useState } from "react";
+import { api } from "../../services/axiosInstance";
+import { useUserContext } from "../../hooks/useUserContext";
 
 const B40Transaction = () => {
+  const [transaction, setTransaction] = useState();
+  const { user } = useUserContext();
+
+  useEffect(() => {
+    api
+      .get(`/student/transaction/wallet/${user?.id}`)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
   return (
     <View style={[globals.container]}>
       <TransactionContainer>
